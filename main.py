19a74bf1f07e9f8186b03aa426c80ddcd6f9cb72
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-11-09 10:14:01 -0300

    Change the GUI to CLI

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
+import argparse
+from enum import StrEnum
 import random
+import typing
+
 import operacoes
 
 
-if __name__ == "__main__":
-    # Solicita o nome do arquivo ao usuário
-    nome_arquivo = input("Digite o nome do arquivo: ")
+class Heuristics(StrEnum):
+    MST = "agm"
+    CLOSEST_NEIGHBOR = "nn"
+    CHEAPEST_INSERTION = "ci"
 
-    # Chamada da função para ler as coordenadas
-    matriz_coordenadas = operacoes.leitor_coordenadas_tsp(nome_arquivo)
+    def get_heuristic_function(self)->typing.Callable:
+        function_mapping = {
+            Heuristics.MST: operacoes.heuristica_arvore_geradora_minima,
+            Heuristics.CLOSEST_NEIGHBOR: operacoes.heuristica_vizinho_mais_proximo,
+            Heuristics.CHEAPEST_INSERTION: operacoes.heuristica_insercao_mais_barata
+        }
+        return function_mapping[self]
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Execução do TSP com heurísticas")
+    parser.add_argument(
+        "--filename",
+        type=str,
+        help="Nome do arquivo de entrada"
+    )
+    parser.add_argument(
+        "--heuristic",
+        type=Heuristics,
+        help="Heurística a ser utilizada"
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="nome do arquivo de saida para os resultados do benchmark",
+        default="resultados_tsp.txt"
+    )
+    return parser
+
+if __name__ == "__main__":
+    p = create_parser()
+    args = p.parse_args()
+    matriz_coordenadas = operacoes.leitor_coordenadas_tsp(args.filename)
     print(matriz_coordenadas)
 
-    otimo_instancia_dada = operacoes.obter_otimo('otimos.csv', nome_arquivo)
+    otimo_instancia_dada = operacoes.obter_otimo('otimos.csv', args.filename)
     print(f"Ótimo da instância dada: {otimo_instancia_dada}")
 
     # Seleciona um índice aleatório para a cidade inicial
@@ -18,29 +53,13 @@ if __name__ == "__main__":
     cidade_inicial = matriz_coordenadas[indice_cidade_inicial]
     print(f"Cidade inicial aleatória: {cidade_inicial}, cujo índice é {indice_cidade_inicial}")
 
-    # Menu para selecionar a heurística
-    print("\nSelecione a heurística para o TSP:")
-    print("1. Vizinho Mais Próximo")
-    print("2. Inserção Mais Barata")
-    print("3. Árvore Geradora Mínima")
-
-    escolha = input("Digite o número da heurística desejada: ")
-
-    # Define a heurística escolhida com base na entrada do usuário
-    if escolha == '1':
-        heuristica = operacoes.heuristica_vizinho_mais_proximo
-    elif escolha == '2':
-        heuristica = operacoes.heuristica_insercao_mais_barata
-    elif escolha == '3':
-        heuristica = operacoes.heuristica_arvore_geradora_minima
-    else:
-        print("Escolha inválida! Selecione uma opção de 1 a 3.")
-        exit(1)
+    heuristic: Heuristics = args.heuristic
+    heuristic_function = heuristic.get_heuristic_function()
 
     # Nome do arquivo de saída
     arquivo_saida = 'resultado_tsp.txt'
 
     # Executa o TSP com a heurística escolhida
-    operacoes.executar_tsp(matriz_coordenadas, cidade_inicial, arquivo_saida, otimo_instancia_dada, heuristica)
+    operacoes.executar_tsp(matriz_coordenadas, cidade_inicial, arquivo_saida, otimo_instancia_dada, heuristic_function)
 
     print(f"Resultados salvos em {arquivo_saida}")
