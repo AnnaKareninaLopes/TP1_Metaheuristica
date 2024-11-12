48b9567442c7289dc516ffc68d78e815972635e1
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-11-12 19:35:37 -0300

    Squashed commit of the following:
    commit 807b012aadfb9b77bd193cba23012e7164c8c563
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Tue Nov 12 19:32:34 2024 -0300
    
        Add instance ch150
    
    commit 09d69a794fecb1e1ca9942514026db92fe5ac788
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Mon Nov 11 14:09:32 2024 -0300
    
        Fix: typo error in directory path
    
    commit d547c31212998d16a4080992b9d627ab834441b0
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Mon Nov 11 14:04:58 2024 -0300
    
        update code to storage each result by instance file
    
    commit fb9bab8aa1d51a80d7c09e46adf1e523d5442cf2
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Mon Nov 11 11:01:37 2024 -0300
    
        Fix: update parameters parsing in main
    
    commit 92ebae4d767aadb8ad7e3457a92be5b3cfc95abb
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sun Nov 10 23:03:19 2024 -0300
    
        Change re.sub to os.path.basename to improve compability between different OS
    
    commit 32ffc3c5f9f16b78f970dae246dee7ee13ee132a
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sun Nov 10 21:16:07 2024 -0300
    
        Change StrEnum to use multiple inheritance to improve Python version compatibility.
    
    commit 952475a6bc0ed4795fc30550d320e2a76934016c
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 20:08:41 2024 -0300
    
        remove debug if statement
    
    commit fff950faba17e82eaf92282ca9812c8879643799
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 20:07:06 2024 -0300
    
        Suppress output of heuristic executions in run_benchmark.sh
    
    commit 799aed41c6f24d15054279a1397bcd8d0d7654b0
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 20:06:44 2024 -0300
    
        remove exe debug function
    
    commit cbb61ae473c2b608ba13813a8e574f020486ec6a
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 19:55:57 2024 -0300
    
        Fix: correctly concatenate results across different executions
    
    commit b242313087de56c7a26ea8e88c61e9de37093037
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 19:33:49 2024 -0300
    
        change 172 ycoord
    
    commit c7931c877904ed8234e74e18b06d6cd201580362
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 18:06:24 2024 -0300
    
        Add bashscript
    
    commit 5cc919e15e30bd8fb920c6abcfb63d6c91656a54
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 18:06:14 2024 -0300
    
        Add regex to extract only the instance file name of the nome_instancia
    
    commit ad93e2eb9c2a82b45371e165bbbc7269e0846a3b
    Author: Vitor Rodrigues <vitorrodrys@gmail.com>
    Date:   Sat Nov 9 16:59:03 2024 -0300
    
        Add instances examples in a separeted folder

---
+++ run_benchmark.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+function run_benchmark(){
+    output_dir=$1
+    instances_dir=$2
+    start_node=$3
+
+    # Create output directory
+    mkdir -p $output_dir
+
+    for file in $(ls $instances_dir); do
+        echo "Running to instance $file"
+        echo "running now agm heuristic"
+        instance_directory="$output_dir/$(echo $file | egrep '\..+$')"
+        mkdir -p $instance_directory
+        python3 main.py $instances_dir/$file $instance_directory/result.$file.agm.txt agm $start_node > /dev/null 2>&1
+        echo "running now nn heuristic"
+        python3 main.py $instances_dir/$file $instance_directory/result.$file.nn.txt nn $start_node > /dev/null 2>&1
+        echo "running now ci heuristic"
+        python3 main.py $instances_dir/$file $instance_directory/result.$file.ci.txt ci $start_node > /dev/null 2>&1
+    done
+
+}
+
+function run_benchmark_and_storage_output_by_instance_file(){
+    output_dir=$1
+    instances_dir=$2
+    start_node=$3
+
+    agm_output_dir="$output_dir/agm"
+    nn_output_dir="$output_dir/nn"
+    ci_output_dir="$output_dir/ci"
+    # Create output directory
+    mkdir -p $agm_output_dir
+    mkdir -p $nn_output_dir
+    mkdir -p $ci_output_dir
+
+    for file in $(ls $instances_dir); do
+        echo "Running to instance $file"
+        echo "running now agm heuristic"
+        python3 main.py $instances_dir/$file $agm_output_dir/results.txt agm $start_node > /dev/null 2>&1
+        echo "running now nn heuristic"
+        python3 main.py $instances_dir/$file $nn_output_dir/results.txt nn $start_node > /dev/null 2>&1
+        echo "running now ci heuristic"
+        python3 main.py $instances_dir/$file $ci_output_dir/results.txt ci $start_node > /dev/null 2>&1
+    done
+
+}
+
+function help(){
+    echo "Usage: $0 <output_dir> <instances_dir> <start_node>"
+    echo "output_dir: directory to save the results"
+    echo "instances_dir: directory with the instances"
+    echo "start_node: initial node that each heuristic will start"
+    exit $1
+}
+
+function main(){
+
+    if [ $1 == "-h" ]; then
+        help 0
+    elif [ $# -ne 3 ]; then
+        help 1
+    fi
+
+
+    output_dir=$1
+    instances_dir=$2
+    start_node=$3
+
+    run_benchmark_and_storage_output_by_instance_file $output_dir $instances_dir $start_node
+
+}
+
+main $@
