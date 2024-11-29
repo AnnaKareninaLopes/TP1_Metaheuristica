5ae6c001a642f9b350e94da18c22e818104fa0b0
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-11-29 09:59:32 -0300

    Implement hill clibing local search

---
+++ local_search/hill_climbing.py
@@ -0,0 +1,29 @@
+from constructive_heuristics import ConstructiveHeuristic
+from operacoes import calcular_funcao_objetivo
+
+from .neighborhood_structure import NeighborhoodStructure
+
+class HillClimbing:
+
+    def __init__(
+        self,
+        constructive_heuristic: ConstructiveHeuristic,
+        neighborhood_structure: NeighborhoodStructure
+    ):
+        self.__initial_heuristic = constructive_heuristic
+        self.__neighborhood_structure = neighborhood_structure
+
+    def solve(self) -> tuple[float, list[int]]:
+        """
+        Solve the TSP problem using the hill climbing algorithm
+        returns a tuple with the cost of the solution and a list that is the
+        encode of the path
+        """
+        initial_solution = self.__initial_heuristic.solve()
+        best_solution = initial_solution
+        best_cost = calcular_funcao_objetivo(best_solution)
+        while True:
+            if not self.__neighborhood_structure.generate_neighborhood(best_solution):
+                break
+            best_cost = calcular_funcao_objetivo(best_solution)
+        return best_cost, best_solution
