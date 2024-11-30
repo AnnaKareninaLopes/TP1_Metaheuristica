f6ba572686686f70a9b207bd92ce6e2170940c10
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-11-30 15:35:51 -0300

    Add three opt (work in progress)

--- neighborhood_structure.py
+++ neighborhood_structure.py
@@ -41,6 +41,27 @@ class TwoOpt(NeighborhoodStructure):
                     yield solution
                     solution[i+1], solution[j] = solution[j], solution[i+1]
 
+class ThreeOpt(NeighborhoodStructure):
+
+    def __3opt_permutations(self, solution: list[int], i: int, j: int, k: int) -> Generator[list[int], None, None]:
+        for fromi in [i, i+1]:
+            for fromj in [j, j+1]:
+                for fromk in [k, k+1]:
+                    solution[i], solution[i+1] = solution[fromi], solution[fromj]
+                    solution[j], solution[j+1] = solution[fromj], solution[fromk]
+                    solution[k], solution[k+1] = solution[fromk], solution[fromi]
+                    yield solution
+                    
+    def enumerate_neighbors(self, solution: list[int]) -> Generator[list[int], None, None]:
+        for i, _ in enumerate(solution[:-1]):
+            for j, _ in enumerate(solution[:-1]):
+                for k, _ in enumerate(solution[:-1]):
+                    if all([i != aux and i+1 != aux+1 for aux in [j, k]]):
+                        solution[i+1], solution[j] = solution[j], solution[i+1]
+                        solution[j+1], solution[k] = solution[k], solution[j+1]
+                        yield solution
+                        solution[j+1], solution[k] = solution[k], solution[j+1]
+                        solution[i+1], solution[j] = solution[j], solution[i+1]
 class NeighborhoodStructureEnum(str, Enum):
     SWAPDISTANT = "SwapDistant"
     DELETEINSERT = "DeleteInsert"
