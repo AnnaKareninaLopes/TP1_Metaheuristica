e34d8a638f301968c4b68678fa74399f94cc9444
Vitorrodrys <Vitorrodrys@gmail.com>
2024-11-30 16:12:40 -0300

    remove delete insert and swap distant

--- neighborhood_structure.py
+++ neighborhood_structure.py
@@ -11,25 +11,6 @@ class NeighborhoodStructure(ABC):
         specify neighborhood structure
         """
 
-class SwapDistant(NeighborhoodStructure):
-
-    def enumerate_neighbors(self, solution: list[int]) -> Generator[list[int], None, None]:
-        for index, _ in enumerate(solution):
-            for index2, _ in enumerate(solution):
-                solution[index], solution[index2] = solution[index2], solution[index]
-                yield solution
-                solution[index], solution[index2] = solution[index2], solution[index]
-
-class DeleteInsert(NeighborhoodStructure):
-
-    def enumerate_neighbors(self, solution: list[int]) -> Generator[list[int], None, None]:
-        for index, element in enumerate(solution):
-            for index2, _ in enumerate(solution):
-                if index != index2:
-                    cp = solution.copy()
-                    cp.pop(index)
-                    cp.insert(index2, element)
-                    yield cp
 
 class TwoOpt(NeighborhoodStructure):
 
@@ -40,37 +21,11 @@ class TwoOpt(NeighborhoodStructure):
                     solution[i+1], solution[j] = solution[j], solution[i+1]
                     yield solution
                     solution[i+1], solution[j] = solution[j], solution[i+1]
-
-class ThreeOpt(NeighborhoodStructure):
-
-    def __3opt_permutations(self, solution: list[int], i: int, j: int, k: int) -> Generator[list[int], None, None]:
-        for fromi in [i, i+1]:
-            for fromj in [j, j+1]:
-                for fromk in [k, k+1]:
-                    solution[i], solution[i+1] = solution[fromi], solution[fromj]
-                    solution[j], solution[j+1] = solution[fromj], solution[fromk]
-                    solution[k], solution[k+1] = solution[fromk], solution[fromi]
-                    yield solution
-                    
-    def enumerate_neighbors(self, solution: list[int]) -> Generator[list[int], None, None]:
-        for i, _ in enumerate(solution[:-1]):
-            for j, _ in enumerate(solution[:-1]):
-                for k, _ in enumerate(solution[:-1]):
-                    if all([i != aux and i+1 != aux+1 for aux in [j, k]]):
-                        solution[i+1], solution[j] = solution[j], solution[i+1]
-                        solution[j+1], solution[k] = solution[k], solution[j+1]
-                        yield solution
-                        solution[j+1], solution[k] = solution[k], solution[j+1]
-                        solution[i+1], solution[j] = solution[j], solution[i+1]
 class NeighborhoodStructureEnum(str, Enum):
-    SWAPDISTANT = "SwapDistant"
-    DELETEINSERT = "DeleteInsert"
     TWOOPT = "TwoOpt"
 
     def get_neighborhood_structure_class(self) -> Type[NeighborhoodStructure]:
         neighborhood_structure_mapping = {
-            NeighborhoodStructureEnum.SWAPDISTANT: SwapDistant,
-            NeighborhoodStructureEnum.DELETEINSERT: DeleteInsert,
             NeighborhoodStructureEnum.TWOOPT: TwoOpt
         }
         return neighborhood_structure_mapping[self]
