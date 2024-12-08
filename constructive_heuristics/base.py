fc8c5f21d57ba1075ba807cfd36a7676d975e2ed
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-12-08 11:03:50 -0300

    Add ci and nn constructive heuristics

--- constructive_heuristics/base.py
+++ constructive_heuristics/base.py
@@ -2,6 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class ConstructiveHeuristic(ABC):
+
+    def criar_dicionario_cidades_ordenadas(self, matriz_distancias):
+        dicionario = {}
+
+        for i in range(len(matriz_distancias)):
+            distancias = [(j, matriz_distancias[i][j]) for j in range(len(matriz_distancias)) if i != j]
+            distancias_ordenadas = sorted(distancias, key=lambda x: x[1])
+            dicionario[i + 1] = [cidade + 1 for cidade, _ in distancias_ordenadas]  # Adiciona +1 para ajustar os índices
+        return dicionario
+
     @abstractmethod
     def __init__(self, cordenates: list[list[int, int]], first_city: int):
         pass
--- constructive_heuristics/nn.py
+++ constructive_heuristics/nn.py
@@ -1,6 +1,38 @@
 from .base import ConstructiveHeuristic
 
-
 class NearestNeighbor(ConstructiveHeuristic):
-    def solve(self):
-        pass
+
+    def __init__(self, cordenates: list[list[int, int]], first_city: int):
+        self.cordenates = cordenates
+        self.first_city = first_city
+        self.dicionario_ondenado = self.criar_dicionario_cidades_ordenadas(cordenates)
+
+    def solve(self) -> list[int]:
+        dicionario = self.dicionario_ondenado
+        matriz_distancias = self.cordenates
+        cidade_atual = self.first_city
+        cidade_inicial = self.first_city
+        vetor_solucao = [cidade_atual]
+        distancia_total = 0
+
+        while len(vetor_solucao) < len(dicionario):
+            menor_distancia = float('inf')
+            vizinho_mais_proximo = None
+
+            for vizinho in dicionario[cidade_atual]:
+                if vizinho not in vetor_solucao:
+                    distancia = matriz_distancias[cidade_atual - 1][vizinho - 1]
+                    if distancia < menor_distancia:
+                        menor_distancia = distancia
+                        vizinho_mais_proximo = vizinho
+
+            if vizinho_mais_proximo is not None:
+                vetor_solucao.append(vizinho_mais_proximo)
+                distancia_total += menor_distancia
+                cidade_atual = vizinho_mais_proximo
+
+        # Adiciona a distância de retorno à cidade inicial
+        distancia_total += matriz_distancias[cidade_atual - 1][cidade_inicial - 1]
+        vetor_solucao.append(cidade_inicial)
+
+        return vetor_solucao + [cidade_inicial]
